d31790cd55691dff7463f49724481c6eb6140346
Alejandro <alex.pizano1234@gmail.com>
2022-08-24 17:31:19 -0600

    Playing with built ins

--- logic.js
+++ logic.js
@@ -1,22 +1,42 @@
 
 function removeElement () {
-    rmv=document.getElementById("circle")
+    rmv=document.getElementById('circle')
+
+    console.log(rmv)
     rmv.remove();
 
     
   }
-function addElement () {
-  // crea un nuevo div
-  // y añade contenido
-  number = document.querySelectorAll('#circle');
-  
-  console.log(number)
-  var newDiv = document.createElement("div");
-  newDiv.setAttribute("id","circle")
-  newDiv.innerHTML = number.length
-  // añade el elemento creado y su contenido al DOM
-  var currentDiv = document.getElementById("cirlce1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
+  function addElement () {
+    // crea un nuevo div
+    // y añade contenido
+    circ = document.getElementById('circle')
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("id","circle")
+    if (circ!=null){
+      last=circ.innerHTML
+      newDiv.innerHTML = Number(last)+1
+    }else{
 
+      newDiv.innerHTML = 0
+
+    }
+    var currentDiv = document.getElementById("circle");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+  
+
+  // function addElement () {
+  //   // crea un nuevo div
+  //   // y añade contenido
+  //   number = document.querySelectorAll('#circle');
+  //   console.log(number)
+  //   var newDiv = document.createElement("div");
+  //   newDiv.setAttribute("id","circle")
+  //   newDiv.innerHTML = number.length+1
+  //   // añade el elemento creado y su contenido al DOM
+  //   var currentDiv = document.getElementById("cirlce1");
+  //   document.body.insertBefore(newDiv, currentDiv);
+  // }
+  
 
